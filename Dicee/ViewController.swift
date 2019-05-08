8f8cee41ce05dac58ec8fe96a2566a5f26c6ecc2
jkcon <github@xiixii.pw>
2019-05-08 12:40:33 -0500

    Add random dice roll image update

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    func rollDice () {
+        randomDiceIndex1 = Int.random(in: 1 ... 6)
+        randomDiceIndex2 = Int.random(in: 1 ... 6)
+        
+        print(randomDiceIndex1, randomDiceIndex2)
+        
+        diceImageView1.image  = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image  = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rollDice()
     }
 
+
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        rollDice()
     }
     
 }
